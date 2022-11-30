bdb0008eaa4d7f19b234878df85d4107a40117ab
Ioann <korobkov-1@mail.ru>
2022-12-01 01:37:15 +0300

    Добавили логических тип данных

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Кто нахуй?");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "серега")
+{
+    Console. WriteLine("Ебать, здорово корешок");
+}
+else
+{
+    Console.Write ("Позови Серегу,");
+    Console.WriteLine(username);
+}
